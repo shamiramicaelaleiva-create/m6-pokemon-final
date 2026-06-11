77fd079b5b7c27e2ae452ff377937b1b150d2141
shamiramicaelaleiva <shamiramicaelaleiva@gmailcom>
2026-06-11 15:02:44 +0200

    fix pokemon component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
+import PokemonSearch from './PokemonSearch';
+
+export default function Page() {
+  return <PokemonSearch />;
 }
--- components/PokemonSearch.js
+++ components/PokemonSearch.js
@@ -1,4 +1,3 @@
-import PokemonSearch from "./PokemonSearch";
 'use client';
 
 import { useState } from 'react';
